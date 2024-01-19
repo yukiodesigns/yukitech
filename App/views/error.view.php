7e4d0b46dde988d18a0545b564fe233cae168537
Yukio <elsie.atieno@student.moringaschool.com>
2024-01-19 23:25:08 +0300

    added main views

---
+++ App/views/error.view.php
@@ -0,0 +1,14 @@
+<?php loadPartial('head'); ?>
+<?php loadPartial('navbar'); ?>
+
+<section>
+      <div class="container mx-auto p-4 mt-4">
+         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3"><?= $status?></div>
+         <p class="text-center text-2xl mb-4">
+         <?= $message ?>
+         </p>
+         <a href="/listings" class='block text-center'> Go back to Listings
+      </div>
+      </section>
+
+<?php loadPartial('footer'); ?>
