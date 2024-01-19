26c78e6688855ade332cef109dce8fd743c88bcb
Yukio <elsie.atieno@student.moringaschool.com>
2024-01-19 23:16:22 +0300

    Added routes

---
+++ routes.php
@@ -0,0 +1,7 @@
+<?php
+
+$router->get('/', 'HomeController@index');
+$router->get('/listings', 'ListingController@index');
+$router->get('/listings/create', 'ListingController@create');
+$router->get('/listing/{id}', 'ListingController@show');
+;
